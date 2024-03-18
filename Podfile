a545f1530adbaef86518f915c9f72e5678941967
subhadipverloop <159517480+subhadipverloop@users.noreply.github.com>
2024-03-18 14:51:40 +0530

    done

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'sampleappIOS2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for sampleappIOS2
+pod 'VerloopSDKiOS'
+
+end
---
+++ sampleappIOS2/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  sampleappIOS2
+//
+//  Created by Subhadip Mondal on 18/03/24.
+//
+
+import UIKit
+import VerloopSDKiOS
+import VerloopSDKiOS
+
+class ViewController: UIViewController {
+    
+    let uuid = NSUUID().uuidString
+
+    override func viewDidLoad() {
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickButton(_ sender: Any) {
+        
+        let config = VLConfig(clientId: "subhadipmondal")
+        config.setUserId(userId: uuid)
+        config.setUserName(userName: "IOS "+uuid)
+        config.setUserEmail(userEmail: uuid+"@icloud.com")
+//        config.setUserPhone(userPhone: "+91-"+uuid)
+        config.putCustomField(key: "userID", value: uuid, scope: .ROOM)
+        config.setStaging(isStaging: false)
+        let verloop = VerloopSDK(config: config)
+        let chatController = verloop.getNavController()
+        self.present(chatController, animated: true)
+        
+    }
+    
+}
+
